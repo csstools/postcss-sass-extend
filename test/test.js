a620acaf0effe73c1268bb73fe743e779955a8fa
Jonathan Neal <jonathantneal@hotmail.com>
2015-07-10 00:46:33 -0400

    Update tests

---
+++ test/test.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var postcss = require('postcss');
+var expect  = require('chai').expect;
+
+var plugin = require('../');
+
+var dir = './test/fixtures/';
+var expectedExtension = /\.expected\.css$/;
+
+var test = function (input, output, opts, done) {
+	postcss([ plugin(opts) ]).process(input).then(function (result) {
+		expect(result.css).to.eql(output);
+
+		expect(result.warnings()).to.be.empty;
+
+		done();
+	}).catch(function (error) {
+		done(error);
+	});
+};
+
+fs.readdirSync(dir).filter(function (file) {
+	return expectedExtension.test(file);
+}).forEach(function (expectedFile) {
+	var fixtureFile = expectedFile.replace(expectedExtension, '.css');
+	var fixture = fs.readFileSync(dir + fixtureFile, 'utf8');
+
+	var expected = fs.readFileSync(dir + expectedFile, 'utf8');
+
+	describe(fixtureFile, function () {
+		it('looks like ' + expectedFile, function (done) {
+			test(fixture, expected, { }, done);
+		});
+	});
+});
